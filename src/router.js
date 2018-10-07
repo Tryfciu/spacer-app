fb3ce617b002d5aa7cc1856d46088944045cd90d
Paweł Tryfon <pawelxtryfon@gmail.com>
2018-10-07 20:58:14 +0200

    Add search input.

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Search from './views/Search.vue';
+import About from './views/About.vue';
 
 Vue.use(Router);
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'Search',
+      component: Search,
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About,
     },
   ],
 });
---
+++ src/views/Search.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="wrapper">
+        <div class="search">
+            <label for="search">Search</label>
+            <input
+            id="search" 
+            name="search" 
+            v-model="searchValue"
+            @input="handleInput"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Search',
+    data() {
+        return {
+            searchValue: '',
+        };
+    },
+    methods: {
+        handleInput(e) {
+            console.log(this.searchValue);
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+    .wrapper {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin: 0;
+        padding: 30px;
+        width: 100%;
+    }
+
+    .search {
+        display: flex;
+        flex-direction: column;
+        width: 300px;
+
+        label {
+            font-family: Montserrat, sans-serif;
+        }
+
+        input {
+            height: 30px;
+            border: 0;
+            border-bottom: 1px solid black;
+        }
+    }
+</style>
+
