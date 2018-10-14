013fa36b0b482ea3e2b27b94c4326020e2db8cd1
Paweł Tryfon <pawelxtryfon@gmail.com>
2018-10-14 15:57:53 +0200

    Add background image and logo image.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'linebreak-style': 0,
+    'func-names': ["error","never"],
   },
   parserOptions: {
     parser: 'babel-eslint',
